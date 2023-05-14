cfeaca7647423fb6f35ffa89756eb535d4311ed2
murilo-henrique060 <henrique.henryconde@gmail.com>
2023-05-14 10:45:33 -0300

    index changed

--- index.php
+++ index.php
@@ -1,58 +1,58 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <script src="js/dinamic_table.js"></script>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IFID</title>
+    <title>Criar Cartão</title>
 </head>
 <body>
-    <form action="generate-card.php" enctype="multipart/form-data" method="post">
-        <label for="name">Nome:</label> <br>
-        <input type="text" name="name" id="name" placeholder="Nome" required/> <br>
-        <br>
-        <label for="school_id">Matrícula:</label> <br>
-        <input type="text" name="school_id" id="school_id" placeholder="Matrícula" pattern="[0-9]{11}" title="A matrícula é formada por 11 números" required/> <br>
-        <br>
-        <label for="course">Curso:</label> <br>
-        <select name="course" id="course" required>
-            <option value="Técnico em Agente Comunitário de Saúde">Técnico em Agente Comunitário de Saúde</option>
-            <option value="Técnico em Agrimensura">Técnico em Agrimensura</option>
-            <option value="Técnico em Aquicultura">Técnico em Aquicultura</option>
-            <option value="Técnico em Desenvolvimento de Sistemas">Técnico em Desenvolvimento de Sistemas</option>
-            <option value="Técnico em Design de Móveis e Interiores">Técnico em Design de Móveis e Interiores</option>
-            <option value="Técnico em Edificações">Técnico em Edificações</option>
-            <option value="Técnico em Eletrônica">Técnico em Eletrônica</option>
-            <option value="Técnico em Eletrotécnica">Técnico em Eletrotécnica</option>
-            <option value="Técnico em Estradas">Técnico em Estradas</option>
-            <option value="Técnico em Eventos">Técnico em Eventos</option>
-            <option value="Técnico em Geodésia e Cartografia">Técnico em Geodésia e Cartografia</option>
-            <option value="Técnico em Hospedagem">Técnico em Hospedagem</option>
-            <option value="Técnico em Mecânica">Técnico em Mecânica</option>
-            <option value="Técnico em Metalurgia">Técnico em Metalurgia</option>
-            <option value="Técnico em Mineração">Técnico em Mineração</option>
-            <option value="Técnico em Pesca">Técnico em Pesca</option>
-            <option value="Técnico em Química">Técnico em Química</option>
-            <option value="Técnico em Saneamento">Técnico em Saneamento</option>
-            <option value="Técnico em Segurança do Trabalho">Técnico em Segurança do Trabalho</option>
-            <option value="Técnico em Telecomunicação">Técnico em Telecomunicação</option>
-            <option value="Técnico em Turismo">Técnico em Turismo</option>
-        </select> <br>
-        <br>
-        <label for="state">Estado:</label> <br>
-        <input type="text" name="state" id="state" placeholder="Campus" value="Pará" required/> <br>
-        <br>
-        <label for="type">Tipo:</label> <br>
-        <input type="radio" id="tecnico" name="type" value="Técnico" checked/>
-        <label for="tecnico">Técnico</label><br>
-        <input type="radio" id="graduacao" name="type" value="Graduação">
-        <label for="graduacao">Graduação</label><br>
-        <br>
+    <datalist id="modalidade">
+        <option value="Técnico">
+        <option value="Graduação">
+    </datalist>
 
-        <label for="image">Imagem:</label> <br>
-        <input type="file" name="imageUpload" id="imageUpload" accept="image/*"/> <br>
+    <datalist id="cursos">
+        <option value="Técnico em Agente Comunitário de Saúde">
+        <option value="Técnico em Agrimensura">
+        <option value="Técnico em Aquicultura">
+        <option value="Técnico em Desenvolvimento de Sistemas">
+        <option value="Técnico em Design de Móveis e Interiores">
+        <option value="Técnico em Edificações">
+        <option value="Técnico em Eletrônica">
+        <option value="Técnico em Eletrotécnica">
+        <option value="Técnico em Estradas">
+        <option value="Técnico em Eventos">
+        <option value="Técnico em Geodésia e Cartografia">
+        <option value="Técnico em Hospedagem">
+        <option value="Técnico em Mecânica">
+        <option value="Técnico em Metalurgia">
+        <option value="Técnico em Mineração">
+        <option value="Técnico em Pesca">
+        <option value="Técnico em Química">
+        <option value="Técnico em Saneamento">
+        <option value="Técnico em Segurança do Trabalho">
+        <option value="Técnico em Telecomunicação">
+        <option value="Técnico em Turismo">
+    </datalist>
 
-        <input type="submit" value="Gerar Cartões"> <br>
+    <form action="generate-card.php" method="post" enctype="multipart/form-data">
+        <table id="form">
+            <tr>
+                <th></th>
+                <th>Nome</th>
+                <th>Matrícula</th>
+                <th>Curso</th>
+                <th>Modalidade</th>
+                <th>Foto</th>
+            </tr>
+            <script>addRow("form")</script>
+        </table>
+        <button type="button" onclick="addRow('form')">Adicionar</button>
+        <button type="button" onclick="deleteRow('form')">Remover</button><br>
+        <input type="submit" value="Gerar Cartão">
     </form>
 </body>
 </html>
